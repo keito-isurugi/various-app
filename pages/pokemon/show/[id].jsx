10e190cfdef98d7f73b277436f1c8f761469592e
keito-isurugi <ja224kg@gmail.com>
2023-02-07 07:48:56 +0900

    ポケモン詳細ページレスポンシブ対応

--- pages/pokemon/show/[id].jsx
+++ pages/pokemon/show/[id].jsx
@@ -16,31 +16,36 @@ const PokeShowPage = () => {
   return (
 		<>
 			{/* ヘッダー */}
-			<div className={`bg-gray-100 flex relative ${styles.poke_show_header}`}>
-				<div className='border-2 rounded w-[30px] h-[136px] bg-white border-gray-400 absolute top-[25%] left-[30px] cursor-pointer flex justify-center items-center hover:bg-gray-50' onClick={() => router.push(`/pokemon/show/${Number(pokeId) - 1}`)}>
+			<div className={`bg-gray-100 flex relative pb-10 lg:pb-0 ${styles.poke_show_header}`}>
+				{/* 左矢印 */}
+				<div className='border-2 rounded w-[20px] h-[90px] lg:w-[30px] lg:h-[136px] bg-white border-gray-400 absolute  top-[35%] left-[10px] lg:top-[25%] lg:left-[30px] cursor-pointer flex justify-center items-center hover:bg-gray-50' onClick={() => router.push(`/pokemon/show/${Number(pokeId) - 1}`)}>
 					<img className='transform rotate-[90deg]' src="/img/pokemon/arrow.svg" alt="" />
 				</div>
-				<div className="w-[60%] gap-10 flex mx-auto max-w-[1000px]">
-					<div className="w-[40%] mx-auto">
+				{/* 画像と名前 */}
+				<div className="w-[80%] lg:w-[60%] gap-0 lg:gap-10 flex flex-col lg:flex-row mx-auto max-w-[1000px]">
+
+					<div className="w-[100%] lg:w-[40%] my-0 mx-auto">
 						<img className="w-full" src={pokemonJson[pokeId]?.img} alt={pokemonJson[pokeId]?.name} />
 					</div>
-					<div className='w-[60%] pt-10'>
-						<div className='rounded h-[60%] bg-white shadow-lg p-5'>
-							<p className='font-bold text-xl'>No.{pokemonJson[pokeId]?.no}</p>
-							<p className='font-bold text-3xl'>{pokemonJson[pokeId]?.name}</p>
+
+					<div className='w-[100%] lg:w-[60%] p-0 lg:pt-10 text-center lg:text-left'>
+						<div className='rounded h-[100%] lg:h-[60%] bg-white shadow-lg p-5'>
+							<p className='font-bold text-lg lg:text-xl'>No.{pokemonJson[pokeId]?.no}</p>
+							<p className='font-bold text-2xl lg:text-3xl'>{pokemonJson[pokeId]?.name}</p>
 						</div>
 					</div>
 				</div>
-				<div className='border-2 rounded w-[30px] h-[136px] bg-white border-gray-400 absolute top-[25%] right-[30px] flex justify-center items-center cursor-pointer hover:bg-gray-50' onClick={() => router.push(`/pokemon/show/${Number(pokeId) + 1}`)}>
+				{/* 右矢印 */}
+				<div className='border-2 rounded w-[20px] h-[90px] lg:w-[30px] lg:h-[136px] bg-white border-gray-400 absolute  top-[35%] right-[10px] lg:top-[25%] lg:right-[30px] cursor-pointer flex justify-center items-center hover:bg-gray-50' onClick={() => router.push(`/pokemon/show/${Number(pokeId) + 1}`)}>
 					<img className='transform rotate-[270deg]' src="/img/pokemon/arrow.svg" alt="" />
 				</div>
 			</div>
 
 			{/* メイン */}
 			<div className='w-[90%] mt-10 mb-0 mx-auto max-w-[1000px]'>
-				<div className='flex justify-between'>
+				<div className='flex justify-between flex-col lg:flex-row'>
 					{/* 情報 */}
-					<ul className='w-[49%] border-4 rounded px-16 py-10'>
+					<ul className='w-[100%] lg:w-[49%] border-4 rounded px-4 py-4 lg:px-16 lg:py-10 mb-4'>
 						<li className='mb-10'>
 							<dl className='flex'>
 								<dt className='font-bold text-xl'>分類：</dt>
@@ -75,17 +80,17 @@ const PokeShowPage = () => {
 					</ul>
 
 					{/* ステータス */}
-					<ul className='w-[49%] border-4 rounded p-10'>
+					<ul className='w-[100%] lg:w-[49%] border-4 rounded py-6 px-3 lg:px-10 lg:py-10'>
 						{pokemonJson[pokeId]?.status !== undefined && Object.keys(pokemonJson[pokeId]?.status)?.map((status, index) => (
 							<dl key={index} className='flex mb-6'>
-								<dt className='font-bold text-xl w-[110px]'>{statusName(status)}</dt>
+								<dt className='font-bold lg:text-xl w-[110px]'>{statusName(status)}</dt>
 								<dd className='text-xl font-medium'>
 									<ul className='flex'>
 										{statusBarIsValue(pokemonJson[pokeId]?.status[status]).map((_, index) => (
-											<li key={index} className='bg-amber-400 rounded-[8px] w-[15px] h-[35px] mr-[5px]'></li>
+											<li key={index} className='bg-amber-400 rounded-[8px] w-[10px] h-[25px] lg:w-[15px] lg:h-[35px] mr-[5px]'></li>
 										))}
 										{statusBarNonValue(pokemonJson[pokeId]?.status[status]) !== 0 && statusBarNonValue(pokemonJson[pokeId]?.status[status]).map((_, index) => (
-											<li key={index} className='bg-gray-200 rounded-[8px] w-[15px] h-[35px] mr-[5px]'></li>
+											<li key={index} className='bg-gray-200 rounded-[8px] w-[10px] h-[25px] lg:w-[15px] lg:h-[35px] mr-[5px]'></li>
 										))}
 									</ul>
 								</dd>
@@ -95,8 +100,8 @@ const PokeShowPage = () => {
 				</div>
 				
 				{/* 図鑑 */}
-				<div className='border-4 rounded px-16 py-10 mt-4'>
-					<p className='text-xl font-medium'>
+				<div className='border-4 rounded px-4 py-5 lg:px-16 lg:py-10 mt-4 mb-3'>
+					<p className='text-s lg:text-xl font-medium'>
 						{pokemonJson[pokeId]?.flavor_text}
 					</p>
 				</div>
