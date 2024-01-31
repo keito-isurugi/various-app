c4990f3d9a6a063d4e2e344a2b2a436fb93a61eb
KeitoIsurugi <ja224kg@gmail.com>
2024-02-01 00:25:51 +0900

    npm installエラー修正

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import 'tailwindcss/tailwind.css';
 import type { AppProps } from 'next/app'
 import Layout from '@/components/Layout';
 import { ThemeProvider } from '@mui/material/styles';
-import { StylesProvider } from '@mui/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import Theme from '@/components/Theme';
 import '@/styles/globals.css'
@@ -11,14 +10,12 @@ import '@/styles/pokeslider.css'
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <> 
-    <StylesProvider injectFirst>
       <ThemeProvider theme={Theme}>
         <Layout>
           <CssBaseline />
           <Component {...pageProps} />
         </Layout>
       </ThemeProvider>
-    </StylesProvider>
     </>
   )
 }
