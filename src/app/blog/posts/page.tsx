1f11afc7c523f1193779270817a000c446de12cd
keito-isurugi <ja224kg@gmail.com>
2024-09-29 17:35:41 +0900

    Change the repository name

--- src/app/blog/posts/page.tsx
+++ src/app/blog/posts/page.tsx
@@ -15,8 +15,7 @@ export default function Home() {
 		}
 		getPosts();
 	}, []);
-
-	console.log(posts);
+  
 	return (
 		<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
 			<h1>ブログ一覧</h1>
