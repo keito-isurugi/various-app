a0fb61b06d9dec055a19fe73c4e1575b0afc3a27
keito-isurugi <ja224kg@gmail.com>
2024-10-06 23:07:50 +0900

    Markdownファイル表示お試し修正

--- src/app/demo/page.tsx
+++ src/app/demo/page.tsx
@@ -2,7 +2,8 @@ import fs from "node:fs";
 import path from "path";
 import ReactMarkdown from "react-markdown";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { oneDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
+// import { oneDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import { oneDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 import rehypeRaw from "rehype-raw";
 import remarkGfm from "remark-gfm";
 
@@ -21,14 +22,13 @@ const Home = () => {
 			remarkPlugins={[remarkGfm]}
 			rehypePlugins={[rehypeRaw]}
 			components={{
-				code({ node, inline, className, children, ...props }) {
+				code({ node, className, children, ...props }) {
 					const match = /language-(\w+)/.exec(className || "");
-					return !inline && match ? (
+					return match ? (
 						<SyntaxHighlighter
-							style={oneDark}
+							style={oneDark as any}
 							language={match[1]}
 							PreTag="div"
-							{...props}
 						>
 							{String(children).replace(/\n$/, "")}
 						</SyntaxHighlighter>
