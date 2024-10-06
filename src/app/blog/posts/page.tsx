4917608a237a4e03c22e5d5a5a1bdecc6654329b
keito-isurugi <ja224kg@gmail.com>
2024-10-06 23:34:16 +0900

    ブログ一覧ページSSGに修正

--- src/app/blog/posts/page.tsx
+++ src/app/blog/posts/page.tsx
@@ -1,20 +1,13 @@
-"use client";
-
 import Link from "next/link";
 import React from "react";
-import { useEffect, useState } from "react";
+import { notion } from "@/libs/notion/notionAPI";
 
-export default function Home() {
-	const [posts, setPosts] = useState([]);
-
-	useEffect(() => {
-		async function getPosts() {
-			const res = await fetch("/api/blog/posts");
-			const data = await res.json();
-			setPosts(data.posts);
-		}
-		getPosts();
-	}, []);
+export default async function Home() {
+	// notionAPIでページ一覧取得
+	const response = await notion.databases.query({
+		database_id: process.env.NOTION_DATABASE_ID || "",
+	});
+	const posts: any = response.results;
 
 	return (
 		<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
