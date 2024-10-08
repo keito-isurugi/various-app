8631e8f06519e872ba53faf9f8a221838a6c1c67
keito-isurugi <ja224kg@gmail.com>
2024-10-08 23:17:39 +0900

    ブログ記事一覧ページ、詳細ページスタイル調整

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-// import "../css/globals.css";
+import "../css/globals.css";
 
 const geistSans = localFont({
 	src: "./fonts/GeistVF.woff",
