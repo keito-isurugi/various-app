4690b8ddbe16ad22a66e97db97ba8e7469da230d
keito-isurugi <ja224kg@gmail.com>
2024-12-23 21:00:00 +0900

    aws

---
+++ backend/infra/aws/client.go
@@ -0,0 +1,26 @@
+package aws
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3iface"
+
+	"github.com/keito-isurugi/kei-talk/infra/env"
+)
+
+func NewS3Client(ev *env.Values) (s3iface.S3API, error) {
+	awsConfig := &aws.Config{
+		Region:           aws.String(ev.AwsRegion),
+		S3ForcePathStyle: aws.Bool(true),
+	}
+
+	// エンドポイント設定がある場合のみ、エンドポイントを設定
+	if ev.AwsEndpoint != "" {
+		awsConfig.Endpoint = aws.String(ev.AwsEndpoint)
+	}
+
+	awsSession := session.Must(session.NewSession(awsConfig))
+
+	return s3.New(awsSession), nil
+}
