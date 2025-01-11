1546515417f1dd6de6af9b32bc61c9dbc00f8d79
keito-isurugi <ja224kg@gmail.com>
2025-01-11 14:24:21 +0900

    update tag usecase実装

---
+++ backend/application/tag/update_tag_use_case.go
@@ -0,0 +1,40 @@
+package tag
+
+import (
+	"github.com/labstack/echo/v4"
+
+	tagDomain "github.com/keito-isurugi/kei-talk/domain/tag"
+)
+
+type UpdateTagUseCase interface {
+	Exec(c echo.Context, input TagUseCaseInputDto) error
+}
+
+type updateTagUseCase struct {
+	tagRepo tagDomain.TagRepository
+}
+
+type TagUseCaseInputDto struct {
+	ID   int
+	Name string
+}
+
+func NewUpdateTagUseCase(tagRepo tagDomain.TagRepository) UpdateTagUseCase {
+	return &updateTagUseCase{
+		tagRepo: tagRepo,
+	}
+}
+
+func (uc *updateTagUseCase) Exec(c echo.Context, input TagUseCaseInputDto) error {
+	tag := tagDomain.Tag{
+		ID: input.ID,
+		Name: input.Name,
+	}
+
+	err := uc.tagRepo.UpdateTag(c.Request().Context(), &tag)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
