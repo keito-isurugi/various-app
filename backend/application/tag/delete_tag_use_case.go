172a79b028d6bce2327ddd178e980d7aaf0669b9
keito-isurugi <ja224kg@gmail.com>
2025-01-11 14:24:29 +0900

    delete tag usecase実装

---
+++ backend/application/tag/delete_tag_use_case.go
@@ -0,0 +1,29 @@
+package tag
+
+import (
+	"github.com/labstack/echo/v4"
+
+	tagDomain "github.com/keito-isurugi/kei-talk/domain/tag"
+)
+
+type DeleteTagUseCase interface {
+	Exec(c echo.Context, id int) error
+}
+
+type deleteTagUseCase struct {
+	tagRepo tagDomain.TagRepository
+}
+
+func NewDeleteTagUseCase(tagRepo tagDomain.TagRepository) DeleteTagUseCase {
+	return &deleteTagUseCase{
+		tagRepo: tagRepo,
+	}
+}
+
+func (uc *deleteTagUseCase) Exec(c echo.Context, id int) error {
+	err := uc.tagRepo.DeleteTag(c.Request().Context(), id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
