d04d965a51b0262fad32137ba8ec4b6ecfa26368
keito-isurugi <ja224kg@gmail.com>
2025-01-12 08:58:04 +0900

    tag router実装

--- backend/server/route.go
+++ backend/server/route.go
@@ -5,10 +5,12 @@ import (
 
 	"github.com/aws/aws-sdk-go/service/s3/s3iface"
 	imageApp "github.com/keito-isurugi/kei-talk/application/image"
+	tagApp "github.com/keito-isurugi/kei-talk/application/tag"
 	"github.com/keito-isurugi/kei-talk/infrastructure/env"
 	"github.com/keito-isurugi/kei-talk/infrastructure/postgresql"
 	"github.com/keito-isurugi/kei-talk/infrastructure/postgresql/repository"
 	imagePre "github.com/keito-isurugi/kei-talk/presentation/http/image"
+	tagPre "github.com/keito-isurugi/kei-talk/presentation/http/tag"
 	"github.com/labstack/echo/v4"
 	echoMiddleware "github.com/labstack/echo/v4/middleware"
 	"go.uber.org/zap"
@@ -27,6 +29,7 @@ func SetupRouter(ev *env.Values, dbClient db.Client, _ *zap.Logger, awsClient s3
 	})
 
 	imageRouter(ev, awsClient, api, dbClient)
+	tagRouter(ev, api, dbClient)
 
 	return e
 }
@@ -48,4 +51,38 @@ func imageRouter(ev *env.Values, awsClient s3iface.S3API, eg *echo.Group, dbClie
 	imageGroup.DELETE("/:id", h.DeleteImage)
 	imageGroup.PUT("", h.RegisterImage)
 	imageGroup.PUT("/multi", h.RegisterImages)
+}
+
+func tagRouter(ev *env.Values, eg *echo.Group, dbClient db.Client) {
+	tagRepo := repository.NewTagRepository(dbClient)
+	h := tagPre.NewImageHandler(
+		ev,
+		tagApp.NewListTagsUseCase(tagRepo),
+		tagApp.NewGetTagUseCase(tagRepo),
+		tagApp.NewRegisterTagUseCase(tagRepo),
+		tagApp.NewUpdateTagUseCase(tagRepo),
+		tagApp.NewDeleteTagUseCase(tagRepo),
+	)
+
+	tagGroup := eg.Group("/tags")
+	tagGroup.GET("", h.ListTags)
+	tagGroup.GET("/:id", h.GetTag)
+	tagGroup.POST("", h.RegisterTag)
+	tagGroup.PUT("/:id", h.UpdateTag)
+	tagGroup.DELETE("/:id", h.DeleteTag)
+}
+
+func imageTagRouter(ev *env.Values, eg *echo.Group, dbClient db.Client) {
+    imageTagRepo := repository.NewImageTagRepository(dbClient)
+    h := imageTagPre.NewImageTagHandler(
+        ev,
+        imageTagApp.NewListImageTagsUseCase(imageTagRepo),
+        imageTagApp.NewRegisterImageTagUseCase(imageTagRepo),
+        imageTagApp.NewDeleteImageTagUseCase(imageTagRepo),
+    )
+
+    imageTagGroup := eg.Group("/image-tags")
+    imageTagGroup.GET("", h.ListImageTags)               // 画像とタグの関連リストを取得
+    imageTagGroup.POST("", h.RegisterImageTag)           // 画像にタグを紐づける
+    imageTagGroup.DELETE("/:id", h.DeleteImageTag)       // 紐づけを削除する
 }
