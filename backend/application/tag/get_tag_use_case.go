43c427c839b0d70d6e7715a7d5c94987711886df
keito-isurugi <ja224kg@gmail.com>
2025-01-11 14:24:01 +0900

    get tag usecase実装

---
+++ backend/application/tag/get_tag_use_case.go
@@ -0,0 +1,35 @@
+package tag
+
+import (
+	"github.com/labstack/echo/v4"
+
+	tagDomain "github.com/keito-isurugi/kei-talk/domain/tag"
+)
+
+type GetTagUseCase interface {
+	Exec(c echo.Context, id int) (*TagUseCaseOutputDto, error)
+}
+
+type getTagUseCase struct {
+	tagRepo tagDomain.TagRepository
+}
+
+func NewGetTagUseCase(tagRepo tagDomain.TagRepository) GetTagUseCase {
+	return &getTagUseCase{
+		tagRepo: tagRepo,
+	}
+}
+
+func (uc *getTagUseCase) Exec(c echo.Context, id int) (*TagUseCaseOutputDto, error) {
+	tag, err := uc.tagRepo.GetTag(c.Request().Context(), id)
+	if err != nil {
+		return nil, err
+	}
+
+	dto := TagUseCaseOutputDto{
+		ID:   tag.ID,
+		Name: tag.Name,
+	}
+
+	return &dto, nil
+}
