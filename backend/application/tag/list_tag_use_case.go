7b16166e6e3e0c296bb4089614990321e64983c5
keito-isurugi <ja224kg@gmail.com>
2025-01-11 14:23:45 +0900

    list tag usecase実装

---
+++ backend/application/tag/list_tag_use_case.go
@@ -0,0 +1,43 @@
+package tag
+
+import (
+	"github.com/labstack/echo/v4"
+
+	tagDomain "github.com/keito-isurugi/kei-talk/domain/tag"
+)
+
+type TagUseCaseOutputDto struct {
+	ID   int
+	Name string
+}
+
+type ListTagsUseCase interface {
+	Exec(c echo.Context) (*[]TagUseCaseOutputDto, error)
+}
+
+type listTagsUseCase struct {
+	tagRepo tagDomain.TagRepository
+}
+
+func NewListTagsUseCase(tagRepo tagDomain.TagRepository) ListTagsUseCase {
+	return &listTagsUseCase{
+		tagRepo: tagRepo,
+	}
+}
+
+func (ltuc *listTagsUseCase) Exec(c echo.Context) (*[]TagUseCaseOutputDto, error) {
+	tags, err := ltuc.tagRepo.ListTags(c.Request().Context())
+	if err != nil {
+		return nil, err
+	}
+
+	dto := make([]TagUseCaseOutputDto, len(*tags))
+	for i, tag := range *tags {
+		dto[i] = TagUseCaseOutputDto{
+			ID:   tag.ID,
+			Name: tag.Name,
+		}
+	}
+
+	return &dto, nil
+}
