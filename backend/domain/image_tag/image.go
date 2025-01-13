dcb33becab4c5b04b034ead6eddc99bf9313612f
keito-isurugi <ja224kg@gmail.com>
2025-01-13 23:23:49 +0900

    image_tag domain 実装

---
+++ backend/domain/image_tag/image.go
@@ -0,0 +1,30 @@
+package image_tag
+
+import "time"
+
+type ImageTag struct {
+	ID          int
+	ImageID          int
+	TagID          int
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+	DeletedAt   *time.Time
+}
+
+type ListImageTags []ImageTag
+
+func NewImageTag(
+	id int,
+	imageID int,
+	tagID int,
+	createdAt time.Time,
+	updatedAt time.Time,
+) *ImageTag {
+	return &ImageTag{
+		ID:          id,
+		ImageID:   imageID,
+		TagID: tagID,
+		CreatedAt:   createdAt,
+		UpdatedAt:   updatedAt,
+	}
+}
