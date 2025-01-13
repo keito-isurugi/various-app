e585c9dc7d2fbe85e72b1e21287d1e7bbaf780f5
keito-isurugi <ja224kg@gmail.com>
2025-01-13 23:23:57 +0900

    image_tag repository 実装

---
+++ backend/domain/image_tag/image_repository.go
@@ -0,0 +1,13 @@
+//go:generate mockgen -source=image_repository.go -destination=./mock/image_repository_mock.go
+package image_tag
+
+import (
+	"context"
+)
+
+type ImageTagRepository interface {
+	ListImageTags(ctx context.Context) (*ListImageTags, error)
+	GetImageTag(ctx context.Context, imgTag *ImageTag) (*ImageTag, error)
+	DeleteImageTag(ctx context.Context, imgTag *ImageTag) error
+	RegisterImageTag(ctx context.Context, imgTag *ImageTag) (int, error)
+}
