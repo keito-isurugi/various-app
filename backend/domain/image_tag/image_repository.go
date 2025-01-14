6197daa539b93632264f0d0d85fe585a7f3b5dce
keito-isurugi <ja224kg@gmail.com>
2025-01-14 23:21:55 +0900

    image_tag repository 追加

--- backend/domain/image_tag/image_repository.go
+++ backend/domain/image_tag/image_repository.go
@@ -9,5 +9,7 @@ type ImageTagRepository interface {
 	ListImageTags(ctx context.Context) (*ListImageTags, error)
 	GetImageTag(ctx context.Context, id int) (*ImageTag, error)
 	DeleteImageTag(ctx context.Context, id int) error
+	DeleteMultipleImageTags(ctx context.Context, ids *UpdateMultipleImageTags) error
 	RegisterImageTag(ctx context.Context, imgTag *ImageTag) (int, error)
+	RegisterMultipleImageTags(ctx context.Context, ids *UpdateMultipleImageTags) ([]int, error)
 }
