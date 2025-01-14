86bef5ffcd1df7aaa040ae4522bb0e15297fa59a
keito-isurugi <ja224kg@gmail.com>
2025-01-14 23:22:32 +0900

    image_tag infra 追加

--- backend/infrastructure/postgresql/repository/image_tag_repository.go
+++ backend/infrastructure/postgresql/repository/image_tag_repository.go
@@ -3,9 +3,9 @@ package repository
 import (
 	"context"
 	"fmt"
-
 	imageTagDomain "github.com/keito-isurugi/kei-talk/domain/image_tag"
 	"github.com/keito-isurugi/kei-talk/infrastructure/postgresql"
+	"gorm.io/gorm"
 )
 
 type imageTagRepository struct {
@@ -48,23 +48,6 @@ func (r *imageTagRepository) RegisterImageTag(ctx context.Context, imgTag *image
 	return imgTag.ID, nil
 }
 
-// func (r *imageTagRepository) UpdateTodo(ctx context.Context, todo *imageTagDomain.Image) error {
-// 	var t imageTagDomain.Image
-// 	if err := r.dbClient.Conn(ctx).Where("id", todo.ID).First(&t).Error; err != nil {
-// 		return err
-// 	}
-
-// 	todo.CreatedAt = t.CreatedAt
-
-// 	if err := r.dbClient.Conn(ctx).
-// 		Updates(todo).
-// 		Error; err != nil {
-// 		return err
-// 	}
-
-// 	return nil
-// }
-
 func (r *imageTagRepository) DeleteImageTag(ctx context.Context, id int) error {
 	var it imageTagDomain.ImageTag
 	if err := r.dbClient.Conn(ctx).Where("id", id).First(&it).Error; err != nil {
@@ -74,6 +57,47 @@ func (r *imageTagRepository) DeleteImageTag(ctx context.Context, id int) error {
 	if err := r.dbClient.Conn(ctx).
 		Delete(it).
 		Error; err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *imageTagRepository) RegisterMultipleImageTags(ctx context.Context, ids *imageTagDomain.UpdateMultipleImageTags) ([]int, error) {
+	var insertedIDs []int
+
+	if err := r.dbClient.Conn(ctx).Transaction(func(tx *gorm.DB) error {
+		for _, imageID := range ids.ImageIDs {
+			for _, tagID := range ids.TagIDs {
+				imageTag := imageTagDomain.ImageTag{
+					ImageID: imageID,
+					TagID:   tagID,
+				}
+
+				if err := tx.Create(&imageTag).Error; err != nil {
+					return err
+				}
+				insertedIDs = append(insertedIDs, imageTag.ID)
+			}
+		}
+		return nil
+	}); err != nil {
+		return nil, err
+	}
+
+	return insertedIDs, nil
+}
+
+func (r *imageTagRepository) DeleteMultipleImageTags(ctx context.Context, ids *imageTagDomain.UpdateMultipleImageTags) error {
+	if err := r.dbClient.Conn(ctx).Transaction(func(tx *gorm.DB) error {
+		// 複数条件で削除を実行
+		if err := tx.Where("image_id IN ?", ids.ImageIDs).
+			Where("tag_id IN ?", ids.TagIDs).
+			Delete(&imageTagDomain.ImageTag{}).Error; err != nil {
+			return err
+		}
+		return nil
+	}); err != nil {
 		return err
 	}
 
