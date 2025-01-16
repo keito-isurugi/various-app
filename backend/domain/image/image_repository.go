4e2ba89b5dbef566fa96c83f5c1779f07ef3ee1b
keito-isurugi <ja224kg@gmail.com>
2025-01-16 21:00:00 +0900

    image repository fix

--- backend/domain/image/image_repository.go
+++ backend/domain/image/image_repository.go
@@ -8,6 +8,6 @@ import (
 type ImageRepository interface {
 	ListImages(ctx context.Context) (*ListImages, error)
 	GetImage(ctx context.Context, id int) (*Image, error)
-	DeleteImage(ctx context.Context, id int) error
+	DeleteImage(ctx context.Context, path string) error
 	RegisterImage(ctx context.Context, img *Image) (string, error)
 }
