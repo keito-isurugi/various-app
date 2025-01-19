f1db6ebb0a31687b6be1fc82da8a0224153cbb9b
keito-isurugi <ja224kg@gmail.com>
2025-01-19 15:17:05 +0900

    change list image usecase

--- backend/application/image/list_image_use_case.go
+++ backend/application/image/list_image_use_case.go
@@ -3,6 +3,7 @@ package image
 import (
 	"github.com/labstack/echo/v4"
 
+	tagApp "github.com/keito-isurugi/kei-talk/application/tag"
 	imageDomain "github.com/keito-isurugi/kei-talk/domain/image"
 )
 
@@ -10,6 +11,7 @@ type ImageUseCaseDto struct {
 	ID          int
 	ImagePath   string
 	DisplayFlag bool
+	Tags        []tagApp.TagUseCaseOutputDto
 }
 
 type ListImagesUseCase interface {
@@ -34,10 +36,21 @@ func (ltuc *listImagesUseCase) Exec(c echo.Context) (*[]ImageUseCaseDto, error) 
 
 	dto := make([]ImageUseCaseDto, len(*images))
 	for i, img := range *images {
+		// Tags を DTO に変換
+		tagsDto := make([]tagApp.TagUseCaseOutputDto, len(img.Tags))
+		for j, tag := range img.Tags {
+			tagsDto[j] = tagApp.TagUseCaseOutputDto{
+				ID:   tag.ID,
+				Name: tag.Name,
+			}
+		}
+
+		// 各 Image の情報を DTO に詰める
 		dto[i] = ImageUseCaseDto{
-			ID: img.ID,
-			ImagePath: img.ImagePath,
+			ID:          img.ID,
+			ImagePath:   img.ImagePath,
 			DisplayFlag: img.DisplayFlag,
+			Tags:        tagsDto,
 		}
 	}
 
