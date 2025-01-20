b95f5eeff2d68fe4e6f3f90c87cbe8efbd562d26
keito-isurugi <ja224kg@gmail.com>
2025-01-20 09:13:26 +0900

    change image response

--- backend/presentation/http/image/response.go
+++ backend/presentation/http/image/response.go
@@ -5,7 +5,13 @@ type getImageResponse struct {
 }
 
 type imageResponseModel struct {
-	ID          int    `json:"id"`
-	ImagePath   string `json:"image_path"`
-	DisplayFlag bool `json:"display_flag"`
+	ID          int                `json:"id"`
+	ImagePath   string             `json:"image_path"`
+	DisplayFlag bool               `json:"display_flag"`
+	Tags        []tagResponseModel `json:"tags"`
+}
+
+type tagResponseModel struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
 }
