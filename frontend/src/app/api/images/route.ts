934d3e01a714eac29eaf8a272697843d92a2a297
keito-isurugi <ja224kg@gmail.com>
2025-04-02 21:00:00 +0900

    edit image tag

--- frontend/src/app/api/images/route.ts
+++ frontend/src/app/api/images/route.ts
@@ -1,4 +1,4 @@
-// import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 // import apiClient from "@/libs/apiClient";
 
 // export async function GET() {
@@ -9,3 +9,7 @@
 //     return NextResponse.json({ error: "Failed to fetch images" }, { status: 500 });
 //   }
 // }
+// is not a module.を回避するためのダミーコード
+export async function GET(req: NextRequest) {
+  return NextResponse.json({ message: "Hello, world!" });
+}
